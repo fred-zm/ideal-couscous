77345cc9423f2f2694779fc44f626ad8f243dd1f
Gitea <test@test.test>
2025-06-02 13:47:54 +0200

    Aufgabe 3.3.3 Tkinter Layout

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
 import tkinter as tk
+from tkinter import ttk
 
 root = tk.Tk()
-
-label1 = tk.Label(root, text="Test")
-label1.pack()
-
+root.title("Test Titel")
+root.geometry("400x400")
+root.minsize(width=250, height=250)
+root.maxsize(width=600, height=600)
+#root.resizable(width=False, height=False)
+root.geometry()
+label1 = tk.Label(root, text="Test", bg='light blue')
+label1.pack(side='top', expand=True, fill='y')
+label2 = tk.Label(root, text="TEstTESTTEST", bg='red')
+label2.pack()
+label3 = ttk.Label(root, text="ttk Label")
+label3.pack()
+button1 = tk.Button(root, text="Schließen", command=root.destroy)
+button1.pack()
+button2 = ttk.Button(root, text="ttk Button")
+button2.pack()
 root.mainloop()
 
 print("Ende")
